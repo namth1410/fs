080b2a1c7512ce1bfcbea96f9666c1d5b871b64b
namth1410 <tranhainam1410@gmail.com>
2024-08-05 08:25:03 +0700

    build: release/v1.0.33

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,16 +1,29 @@
 import { useEffect } from "react";
 import { getProducts } from "../../appdata/productsSlice";
 import { useAppDispatch } from "../../appdata/store";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getProducts());
     // eslint-disable-next-line
   }, []);
 
-  return <div className="dark:bg-slate-400">Home</div>;
+  return (
+    <div className="dark:bg-slate-400">
+      Home{" "}
+      <button
+        onClick={() => {
+          navigate("/iphone");
+        }}
+      >
+        hihi
+      </button>
+    </div>
+  );
 }
 
 export default Home;
